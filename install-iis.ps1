19ecf44567e5d9b653c49f01a760d0bd91b4a3e0
bflatts <124673866+bflatts@users.noreply.github.com>
2024-06-04 11:39:26 +0100

    Update install-iis.ps1

--- install-iis.ps1
+++ install-iis.ps1
@@ -1,2 +1,2 @@
-Add-WindowsFeature Web-Server
+Add-WindowsFeature Web-Server, Web-Mgmt-Tools
 Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World from updated host $($env:computername) !"
